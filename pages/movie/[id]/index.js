446707e3057de2bbf2bf581c871984ea83bf9fd5
Temurbek <86568254+timka1996@users.noreply.github.com>
2023-01-13 14:23:30 +0500

    Add files via upload

--- pages/movie/[id]/index.js
+++ pages/movie/[id]/index.js
@@ -36,22 +36,6 @@ const Movie = ({ movie = null }) => {
   return (
     <div className="container max-w-4xl mx-auto pt-6">
       <Meta title={movie.title} />
-      {/* <div className="row mt-5">
-        {movie.people[0].employees.map((e) => (
-          <div className="col-md-2 mt-4" key={e.id}>
-            <div className="px-3">
-              <h3>{e.full_name}</h3>
-              <Image
-                src={e.photo}
-                width={300}
-                height={300}
-                className="rounded-md"
-                alt={e.full_name}
-              />
-            </div>
-          </div>
-        ))}
-      </div> */}
       <div className="px-3">
         <Image
           src={movie.poster}
